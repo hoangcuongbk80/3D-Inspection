dca789b410c49fbebc2e9b5b544289ff5940c605
Hoang Cuong <hoangcuongbk80@gmail.com>
2018-01-25 14:43:27 +0100

    First Commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include "3D_Inspection.h"
+#include "threeD_Inspection.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	3D_Inspection w;
+	threeD_Inspection w;
 	w.show();
 	return a.exec();
 }
